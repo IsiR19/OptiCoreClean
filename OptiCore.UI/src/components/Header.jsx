95a6ceb1512d500bbec0c998d42642e4a33f52e6
Ismail Rajah <ismail.rajah@investec.co.za>
2024-03-19 22:11:11 +0200

    I fixted it

--- OptiCore.UI/src/components/Header.jsx
+++ OptiCore.UI/src/components/Header.jsx
@@ -1,21 +1,31 @@
 import React from 'react';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { BsFillBellFill, BsFillEnvelopeFill, BsPersonCircle, BsSearch, BsJustify } from 'react-icons/bs';
+import './Header.scss'; // Make sure to create a Header.scss file with the styles
 
 function Header({ OpenSidebar }) {
   return (
-    <header className='d-flex justify-content-between align-items-center px-3 py-2 shadow' style={{ backgroundColor: '#263043', color: '#9e9ea4' }}>
-      <div className='menu-icon d-md-none'>
-        <BsJustify className='icon' onClick={OpenSidebar} style={{ cursor: 'pointer' }} />
-      </div>
-      <div className='header-left d-none d-md-flex'>
-        <BsSearch className='icon' />
-      </div>
-      <div className='header-right d-flex align-items-center'>
-        <BsFillBellFill className='icon text-secondary me-3'/>
-        <BsFillEnvelopeFill className='icon text-secondary me-3'/>
-        <BsPersonCircle className='icon text-secondary'/>
-      </div>
-    </header>
+    <Navbar expand="lg" variant="light" className="header">
+      <Container fluid>
+        <Nav.Link onClick={OpenSidebar} className="menu-icon">
+          <BsJustify />
+        </Nav.Link>
+        <Navbar.Brand href="#home" className="header-left">
+          <BsSearch />
+        </Navbar.Brand>
+        <Nav className="header-right">
+          <Nav.Link href="#notifications">
+            <BsFillBellFill />
+          </Nav.Link>
+          <Nav.Link href="#messages">
+            <BsFillEnvelopeFill />
+          </Nav.Link>
+          <Nav.Link href="#profile">
+            <BsPersonCircle />
+          </Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
